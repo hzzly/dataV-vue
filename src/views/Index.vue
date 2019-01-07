a09ce656b338415d80cfe4bd21aa73e9123c2747
黄敬仁 <hjingren@aliyun.com>
2019-01-07 23:33:20 +0800

    :lipstick: grid layout

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -4,6 +4,8 @@
     <div class="top-left"></div>
     <div class="top-center"></div>
     <div class="top-right"></div>
+    <div class="bottom-left"></div>
+    <div class="bottom-right"></div>
   </div>
 </template>
 
@@ -28,26 +30,34 @@ export default {
   display: grid;
   grid-template-columns: repeat(8, 1fr);
   grid-template-rows: 50px repeat(4, 1fr);
-  // grid-template-rows: 50px repeat(3, 1fr) 100px repeat(3, 1fr);
   grid-gap: 6px;
   padding: 6px;
   color: #37d1ff;
-  // background:rgba(30, 35, 52, 0.8);
   z-index: 99;
   .header {
     grid-area: 1 / 1 / span 1 / span 8; // grid-row-start,grid-column-start,grid-row-end,grid-column-end
   }
+  .top-left,
+  .top-center,
+  .top-right,
+  .bottom-left,
+  .bottom-right {
+    background: rgba(40, 47, 56, .8);
+  }
   .top-left {
     grid-area: 2 / 1 / span 2 / span 3;
-    background: rgba(40, 47, 56, .7);
   }
   .top-center {
     grid-area: 2 / 4 / span 2 / span 2;
-    background: rgba(40, 47, 56, .7);
   }
   .top-right {
     grid-area: 2 / 6 / span 2 / span 3;
-    background: rgba(40, 47, 56, .7);
+  }
+  .bottom-left {
+    grid-area: 4 / 1 / span 2 / span 5;
+  }
+  .bottom-right {
+    grid-area: 4 / 6 / span 2 / span 3;
   }
 }
 </style>
